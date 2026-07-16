14462c81545e9b28eb075753b970a6d548efd124
FrankOcrospomaUgaz <anthony.2005.rap@gmail.com>
2026-07-16 09:09:57 -0500

    feat: actualizar filtros de fecha en rentabilidadPaymentGroupsByStatus para usar cuotas

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2225,10 +2225,14 @@ class WebController extends Controller
     {
         $payments = Payment::active()
             ->when($request->start_date_1, function ($query, $startDate) {
-                return $query->whereDate('date', '>=', $startDate);
+                return $query->whereHas('quota', function ($q) use ($startDate) {
+                    return $q->whereDate('date', '>=', $startDate);
+                });
             })
             ->when($request->end_date_1, function ($query, $endDate) {
-                return $query->whereDate('date', '<=', $endDate);
+                return $query->whereHas('quota', function ($q) use ($endDate) {
+                    return $q->whereDate('date', '<=', $endDate);
+                });
             })
             ->whereHas('quota', function ($q) {
                 return $q->where('paid', 1);
